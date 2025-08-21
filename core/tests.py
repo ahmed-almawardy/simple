c4e1fdfd72a35f4dba3a78ffe3d4dd2d405bb020
ahmed.mohamed <ahmedelmawrdy2@gmail.com>
2025-08-21 21:46:24 +0300

    Mock tests

--- core/tests.py
+++ core/tests.py
@@ -1,2 +1,21 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
 
-# Create your tests here.
+
+class CoreTestCase(TestCase):
+
+    def test_true_success(self):
+        self.assertTrue(True)
+    
+    def test_true_failed(self):
+        self.assertTrue(False)
+    
+    def test_raise_error_success(self):
+        with self.assertRaises(ValidationError):
+            raise ValidationError('Raised')
+    
+    def test_raise_error_Failed(self):
+        with self.assertRaises(ValidationError):
+            pass
+    
+
