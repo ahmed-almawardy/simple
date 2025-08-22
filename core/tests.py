9064d83c4ed38b0b25d11816aa82853687c834b0
ahmed.mohamed <ahmedelmawrdy2@gmail.com>
2025-08-22 22:42:13 +0300

    Mocking skipping failed tests

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,4 @@
+from unittest import skipIf, skipUnless
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
@@ -7,13 +8,14 @@ class CoreTestCase(TestCase):
     def test_true_success(self):
         self.assertTrue(True)
     
+    @skipUnless(False, 'mocking skipping a failed test')
     def test_true_failed(self):
         self.assertTrue(False)
     
     def test_raise_error_success(self):
         with self.assertRaises(ValidationError):
             raise ValidationError('Raised')
-    
+    @skipIf(True, 'mocking skipping a failed test')
     def test_raise_error_Failed(self):
         with self.assertRaises(ValidationError):
             pass
